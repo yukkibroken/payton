558225d68d7e0d04c8e041935aa923082abe0a6f
trpsl <trpsl755@gmail.com>
2019-11-24 18:19:52 +0100

    payton: enable eis, disable ois

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -16,6 +16,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.audio.fluence.speaker=true \
     persist.vendor.audio.fluence.voicecall=true
 
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.camera.eis.enable=1 \
+    persist.vendor.camera.ois.disable=1
+
 # FM
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.fm.use_audio_session=true
