d3c4696503f8c023e480b7013bc46c6558cc8c41
cool585 <dhineshcool585@gmail.com>
2021-02-24 21:01:22 +0530

    payton:build eas kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,7 +30,7 @@ BOARD_HAVE_QCOM_FM := true
 BOARD_HAS_QCA_FM_SOC := "cherokee"
 
 # Kernel
-TARGET_KERNEL_CONFIG := lineageos_payton_defconfig
+TARGET_KERNEL_CONFIG := cybertron_defconfig
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/touch.xml
