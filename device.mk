d41cc66e5fde60ab7e5556eb3b30b8fe1e74e45b
YukkiZx <marcelo.kira159@gmail.com>
2021-03-01 16:20:25 +0000

    payton: Add maintainer & device info

--- device.mk
+++ device.mk
@@ -18,7 +18,9 @@
 $(call inherit-product-if-exists, vendor/motorola/payton/payton-vendor.mk)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-ancient
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
